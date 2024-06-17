d9e399eac69ad178ed5044b0a77d6dc4e41a46dd
Niete <nieteis@gmail.com>
2024-06-17 22:12:50 +0100

    Removed base code and added a test hello world message with tailwind styling to test that config was successful

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import Animated, {
   useAnimatedStyle,
   Easing,
 } from "react-native-reanimated";
-import { View, Button } from "react-native";
+import { View, Button, Text } from "react-native";
 
 export default function AnimatedStyleUpdateExample(props) {
   const randomWidth = useSharedValue(10);
@@ -21,26 +21,8 @@ export default function AnimatedStyleUpdateExample(props) {
   });
 
   return (
-    <View
-      style={{
-        flex: 1,
-        alignItems: "center",
-        justifyContent: "center",
-        flexDirection: "column",
-      }}
-    >
-      <Animated.View
-        style={[
-          { width: 100, height: 80, backgroundColor: "black", margin: 30 },
-          style,
-        ]}
-      />
-      <Button
-        title="toggle"
-        onPress={() => {
-          randomWidth.value = Math.random() * 350;
-        }}
-      />
+    <View className="bg-red-100">
+      <Text>Hello World!</Text>
     </View>
   );
 }
